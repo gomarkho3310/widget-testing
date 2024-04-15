24c7e42065b4699c6899f43c1cb3d575e9f64d65
gomarkho3310 <rkazmi@gomarkho.com>
2024-04-15 14:18:37 +0500

    build

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,32 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const widgetDivs = document.querySelectorAll(".nicoraynaud-finance-widget");
 const widgetBgColor = document
   .querySelector("script[data-widget-bg-color]")
   .getAttribute("data-widget-bg-color");
 
-widgetDivs.forEach((div) => {
-  ReactDOM.createRoot(div).render(
-    <React.StrictMode>
-      <App bgColor={widgetBgColor} />
-    </React.StrictMode>
-  );
-});
+// Find the top-most container element in the host page
+const topContainer = document.body.children[0];
+
+// Create a new root element to render the widget
+const widgetRoot = document.createElement("div");
+topContainer.appendChild(widgetRoot);
+
+ReactDOM.createRoot(widgetRoot).render(
+  <React.StrictMode>
+    <App bgColor={widgetBgColor} />
+  </React.StrictMode>
+);
+
+// const widgetDivs = document.querySelectorAll(".nicoraynaud-finance-widget");
+// const widgetBgColor = document
+//   .querySelector("script[data-widget-bg-color]")
+//   .getAttribute("data-widget-bg-color");
+
+// widgetDivs.forEach((div) => {
+//   ReactDOM.createRoot(div).render(
+//     <React.StrictMode>
+//       <App bgColor={widgetBgColor} />
+//     </React.StrictMode>
+//   );
+// });
