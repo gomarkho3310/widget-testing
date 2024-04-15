00af5fb91ef4bf8f6df7b066d5bd0b32fb66e3f5
gomarkho3310 <rkazmi@gomarkho.com>
2024-04-15 15:58:09 +0500

    build

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,12 @@ const widgetBgColor = document
   .querySelector("script[data-widget-bg-color]")
   .getAttribute("data-widget-bg-color");
 
-// Find the top-most container element in the host page
-const topContainer = document.body.children[0];
+// const topContainer = document.body.children[0];
 
-// Create a new root element to render the widget
-const widgetRoot = document.createElement("div");
-topContainer.appendChild(widgetRoot);
+// const widgetRoot = document.createElement("div");
+// topContainer.appendChild(widgetRoot);
 
-ReactDOM.createRoot(widgetRoot).render(
+ReactDOM.createRoot().render(
   <React.StrictMode>
     <App bgColor={widgetBgColor} />
   </React.StrictMode>
