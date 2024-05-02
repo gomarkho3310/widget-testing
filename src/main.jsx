2e31d157511e0906bf5c61309ff03cd5c9f67812
gomarkho3310 <rkazmi@gomarkho.com>
2024-05-02 15:45:59 +0500

    build

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const widgetDivs = document.querySelectorAll(".nicoraynaud-finance-widget");
 // const widgetBgColor = document
 //   .querySelector("script[data-widget-bg-color]")
 //   .getAttribute("data-widget-bg-color");
 
-widgetDivs.forEach((div) => {
-  ReactDOM.createRoot(div).render(
-    <React.StrictMode>
-      <App bgColor={"white"} />
-    </React.StrictMode>
-  );
-});
+const topContainer = document.body.children[0];
+
+const widgetRoot = document.createElement("div");
+topContainer.appendChild(widgetRoot);
+
+ReactDOM.createRoot(widgetRoot).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// const widgetDivs = document.querySelectorAll(".nicoraynaud-finance-widget");
+// const widgetBgColor = document
+//   .querySelector("script[data-widget-bg-color]")
+//   .getAttribute("data-widget-bg-color");
+
+// widgetDivs.forEach((div) => {
+//   ReactDOM.createRoot(div).render(
+//     <React.StrictMode>
+//       <App bgColor={widgetBgColor} />
+//     </React.StrictMode>
+//   );
+// });
