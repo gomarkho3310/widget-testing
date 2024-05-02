a66a1362f56dd5ba9d1c25a75cf40f2252287f47
gomarkho3310 <rkazmi@gomarkho.com>
2024-05-02 15:35:05 +0500

    build

--- src/main.jsx
+++ src/main.jsx
@@ -3,30 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// const widgetBgColor = document
-//   .querySelector("script[data-widget-bg-color]")
-//   .getAttribute("data-widget-bg-color");
+const widgetDivs = document.querySelectorAll(".nicoraynaud-finance-widget");
+const widgetBgColor = document
+  .querySelector("script[data-widget-bg-color]")
+  .getAttribute("data-widget-bg-color");
 
-const topContainer = document.body.children[0];
-
-const widgetRoot = document.createElement("div");
-topContainer.appendChild(widgetRoot);
-
-ReactDOM.createRoot(widgetRoot).render(
-  <React.StrictMode>
-    <App bgColor={"green"} />
-  </React.StrictMode>
-);
-
-// const widgetDivs = document.querySelectorAll(".nicoraynaud-finance-widget");
-// const widgetBgColor = document
-//   .querySelector("script[data-widget-bg-color]")
-//   .getAttribute("data-widget-bg-color");
-
-// widgetDivs.forEach((div) => {
-//   ReactDOM.createRoot(div).render(
-//     <React.StrictMode>
-//       <App bgColor={widgetBgColor} />
-//     </React.StrictMode>
-//   );
-// });
+widgetDivs.forEach((div) => {
+  ReactDOM.createRoot(div).render(
+    <React.StrictMode>
+      <App bgColor={"white"} />
+    </React.StrictMode>
+  );
+});
