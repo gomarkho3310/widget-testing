233294dc48cece883c0bbbfec5bbb2d274b2e87c
gomarkho3310 <rkazmi@gomarkho.com>
2024-05-30 18:10:23 +0500

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/widgetJs.js`,
+        assetFileNames: `assets/widgetCss.[ext]`,
+      },
+    },
+  },
 });
 
 // import { defineConfig } from "vite";
