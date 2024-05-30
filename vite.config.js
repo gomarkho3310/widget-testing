58b687b7ec0669cd3775c21c2c3ed0a5022cfd95
gomarkho3310 <rkazmi@gomarkho.com>
2024-05-30 18:04:32 +0500

    testing

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,30 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/main.jsx"),
-      name: "MyWidget",
-      formats: ["umd"],
-      fileName: (format) => `my-widget.${format}.js`,
-    },
-    rollupOptions: {
-      // external: ["react", "react-dom"],
-      // output: {
-      //   globals: {
-      //     react: "React",
-      //     "react-dom": "ReactDOM",
-      //   },
-      // },
-    },
-  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     lib: {
+//       entry: path.resolve(__dirname, "src/main.jsx"),
+//       name: "MyWidget",
+//       formats: ["umd"],
+//       fileName: (format) => `my-widget.${format}.js`,
+//     },
+//     rollupOptions: {
+//       external: ["react", "react-dom"],
+//       output: {
+//         globals: {
+//           react: "React",
+//           "react-dom": "ReactDOM",
+//         },
+//       },
+//     },
+//   },
+// });
