590780211ad1af174c4a876a5aca06277468962b
gomarkho3310 <rkazmi@gomarkho.com>
2024-06-03 15:54:21 +0500

    Merge branch 'testing'

--- vite.config.js
+++ vite.config.js
@@ -13,29 +13,3 @@ export default defineConfig({
   //   },
   // },
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import path from "path";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     lib: {
-//       entry: path.resolve(__dirname, "src/main.jsx"),
-//       name: "MyWidget",
-//       formats: ["umd"],
-//       fileName: (format) => `my-widget.${format}.js`,
-//     },
-//     rollupOptions: {
-//       external: ["react", "react-dom"],
-//       output: {
-//         globals: {
-//           react: "React",
-//           "react-dom": "ReactDOM",
-//         },
-//       },
-//     },
-//   },
-// });
